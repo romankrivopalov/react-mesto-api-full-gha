f50f993093cd3b917cbbff717c1a8f9bec9ebc8e
laptop <the.red.lemon@yandex.ru>
2023-05-26 16:30:37 +0300

    add url

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,16 +1,20 @@
 export
-const apiSetting = {
-  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-60',
+const authSetting = {
+  baseUrl: 'https://auth.nomoreparties.co',
+  // baseUrl: 'http://localhost:3000',
+  // baseUrl: 'https://api.krivo.mesto.nomoredomains.rocks',
   headers: {
-    authorization: 'bdecdc76-75a5-40e2-94d6-35ac4e7b5bcc',
     'Content-Type': 'application/json'
   }
 }
 
 export
-const authSetting = {
-  baseUrl: 'https://auth.nomoreparties.co',
+const apiSetting = {
+  baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-60',
+  // baseUrl: 'http://localhost:3001',
+  // baseUrl: 'https://krivo.mesto.nomoredomains.rocks',
   headers: {
+    authorization: 'bdecdc76-75a5-40e2-94d6-35ac4e7b5bcc',
     'Content-Type': 'application/json'
   }
 }
