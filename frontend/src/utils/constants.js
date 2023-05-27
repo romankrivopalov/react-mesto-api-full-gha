bd15f1b9c6c18efa4705dd5e5cfe898b93b86fa4
laptop <the.red.lemon@yandex.ru>
2023-05-27 11:32:01 +0300

    change baseUrl

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,8 +1,8 @@
 export
 const authSetting = {
   // baseUrl: 'https://auth.nomoreparties.co',
-  baseUrl: 'http://localhost:3000',
-  // baseUrl: 'https://api.krivo.mesto.nomoredomains.rocks',
+  // baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://api.krivo.mesto.nomoredomains.rocks',
   headers: {
     'Content-Type': 'application/json'
   }
@@ -11,8 +11,8 @@ const authSetting = {
 export
 const apiSetting = {
   // baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-60',
-  baseUrl: 'http://localhost:3000',
-  // baseUrl: 'https://api.krivo.mesto.nomoredomains.rocks',
+  // baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://api.krivo.mesto.nomoredomains.rocks',
   headers: {
     authorization: 'bdecdc76-75a5-40e2-94d6-35ac4e7b5bcc',
     'Content-Type': 'application/json'
