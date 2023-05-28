3c1ad83ac440e7cf4598c9f35b334173e6b9dd4d
laptop <the.red.lemon@yandex.ru>
2023-05-28 09:33:56 +0300

    remove autorization

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -14,7 +14,6 @@ const apiSetting = {
   // baseUrl: 'http://localhost:3000',
   baseUrl: 'https://api.krivo.mesto.nomoredomains.rocks',
   headers: {
-    authorization: 'bdecdc76-75a5-40e2-94d6-35ac4e7b5bcc',
     'Content-Type': 'application/json'
   }
 }
